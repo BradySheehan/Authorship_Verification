a0097b8a467356cdc73d6910e7b7f80fb1af9280
msobocinski <msobo3355@aol.com>
2016-04-12 23:24:37 -0400

    Feature Vector Creation
    I'll have to give you the combined author files for this to work for you

--- text_processing_python/nlp_sandbox.py
+++ text_processing_python/nlp_sandbox.py
@@ -4,9 +4,41 @@ import operator
 import re
 import string
 
-#class Author:
-#	File;
-#	Features; #the features should be a vector we can input to the neural net
+
+# TODO
+	# determine which of the features are useful
+	# do something with the n-grams
+	# determine how to convert non-numeric values
+
+class Author:
+	def __init__(self, name, file):
+		self.name = name;
+		self.file = file;
+		self.features = self.getFeatureVector(file);
+		self.printFeatures();
+		
+	# can either print this all to a file or print it to console
+	def printFeatures(self):
+		for i in range(0, len(self.features)):
+			print self.features[i],
+		print;
+		
+	# creates the vector of features to be plugged into the Neural Net (much more to add)
+	def getFeatureVector(self, file):
+		features = [];
+		features.append(file.averageWordLength);
+		features.append(file.averageSentenceLength);
+		for i in range(0, len(file.frequencies)):
+			features.append(file.frequencies[i]);
+		# for i in range(0, len(file.rarestWords)):
+			# features.append(self.toBinary(file.rarestWords[i]));
+		return features;
+		
+	# this is a pretty dumb, probably useless generic function for converting a string its binary representation
+	# we probably want the type of conversion we did for Simon
+	def toBinary(self, word):
+		converted = ' '.join(format(ord(letter), 'b') for letter in word);
+		print converted;
 
 
 # should we make a class of features and seperate the file itself in a class from the features
@@ -57,12 +89,11 @@ class File:
 		# print "\nTri-grams:", self.triGrams;
 		# print "\nQuad-grams:", self.quadGrams;
 		# print "\nAverage Word Length:", self.averageWordLength, " letters";
-		# print "\nAverage Sentence Length: ", self.averageSentenceLength;
+		print "\nAverage Sentence Length: ", self.averageSentenceLength;
 		# print "\nFILE no punctuation", self.contentNoPunctuation;
 		# print "\nMost Common First Word: ", self.mostCommonFirstWord;
 		# print "\nWords with frequency 50+: ", self.wordFrequency;
 		print "\nRarest Words:", self.rarestWords;
-		print "Number of Words:", self.numWords;
 		# print self.contentNoPunctuation;
 		print "Word Length Frequencies:", self.frequencies;
 		# print self.contentNoPunctuation;
@@ -80,7 +111,7 @@ class File:
 		# i don't think counting words this way will work unless you do it on the no punctuation file
 		# since documents like the constitutition have weird punctuation attached to words
 		# numWords = len(self.content.split(" ")); 
-		print "num words = ", self.numWords
+		# print "Number of Words =", self.numWords;
 		numPeriods = self.content.count(".");
 		averageSentenceLength = self.numWords/numPeriods;
 		return averageSentenceLength;
@@ -93,7 +124,7 @@ class File:
 			contentNoPunctuation = contentNoPunctuation + re.sub('\W', ' ', str.replace(line, '\s+', ' ')).lower();
 		return contentNoPunctuation;
 
-	# average word length of the file
+	# average word length of the entire file
 	def getAverageWordLength(self):
 		words = self.contentNoPunctuation.split(" ");
 		totalLength = 0;
@@ -102,7 +133,7 @@ class File:
 		avgWordLength = totalLength/len(words);
 		return avgWordLength;
 			
-	# frequencies of word lengths over entire document (1-10, 11+)
+	# frequencies of word lengths over entire document (percentage of 1-length words is stored at [0])
 	def getWordLengthFrequencies(self, contentNoPunctuation):
 		self.frequencies = 11 * [0];
 		contentSplit = contentNoPunctuation.split();
@@ -140,7 +171,6 @@ class File:
 		j = 0;
 		nGrams = [];
 		for i in range(j, len(self.content)):
-			# print self.content[i : (i + n)];
 			nGrams.append(self.content[i : (i + n)]);
 			j = j + 1;
 		return nGrams;
@@ -168,12 +198,6 @@ class File:
 			if value >= frequency:
 				wordFreq.update({key:value});
 		return wordFreq;
-		
-	# generic function for converting a string its binary representation
-	# don't know if we want to do that or the type of conversion we did for Simon
-	def toBinary(self, word):
-		converted = ' '.join(format(ord(letter), 'b') for letter in word);
-		print converted;
 
 	# get all the words in this file that occur between 15 and 25 times
 	def getRarestWords(self, wordCount):
@@ -184,7 +208,6 @@ class File:
 			# str1 = re.match('\w+', ''.join(a), re.IGNORECASE);
 			# if str1:
 				# rarestN.append(sortedWordCount[i]);
-				
 		for i in wordCount:
 			if wordCount[i] >= 15 and wordCount[i] <= 25:
 				rarestN.append(i);
@@ -194,9 +217,10 @@ class File:
 # file = File("Victorian_novels_from_PJ/Collins_50Anton.txt");
 # file.printFields();
 
-# authors = ['Bronte','Collins','Dickens','Ibsen','James','Jewett','Meredith','Phillips','Shaw','Thackeray','Trollope','Wharton'];
-# for name in authors:
- 	# print "NAME: " + name;
- 	# file = File("/Users/matthewsobocinski/Desktop/output/" + name + ".txt");
- 	# file.printFields();
- 	# print;
+authors = ['Bronte','Collins','Dickens','Ibsen','James','Jewett','Meredith','Phillips','Shaw','Thackeray','Trollope','Wharton'];
+for name in authors:
+	print "NAME: " + name;
+	file = File("/Users/matthewsobocinski/Desktop/Folders/AI/Project/output/" + name + ".txt");
+	# file.printFields();
+	author = Author(name, file);
+	print;
