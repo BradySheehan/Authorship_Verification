7674cafe00ecf82bcc7e573505614cbcdfa572ea
Brady Sheehan <Bradya.sheehan@gmail.com>
2016-04-24 21:04:58 -0400

    Updated ppt presentation

--- neural_network_matlab/network_parameter_testing.m
+++ neural_network_matlab/network_parameter_testing.m
@@ -1,19 +1,18 @@
-function result = network_parameter_testing()
-dbstop if error
+function network_parameter_testing()
 [in, out] = processdata();
-trainFcn = {'trainscg', 'traincgb', 'traincgf', 'traincgp'};
+trainFcn = {'trainscg','traincgb','traincgf','traincgp','trainlm','trainbr','trainrp'};
 hiddenLayerSize = [10, 12, 15, 20, 25, 35, 60, 75, 100, 250];
-save('testing_data', 'in', 'out', 'trainFcn', 'hiddenLayerSize');
 ind = 1;
 for ii = 1:length(trainFcn)
     t = trainFcn{1, ii};
     for jj = 1:length(hiddenLayerSize)
         h = hiddenLayerSize(1, jj);
-        [net,outputs, performance, percentCorrect, trainFcn2, hiddenLayerSize2] = pattern_network(in, out, t, h);
-        save(sprintf('%s%d', '2network', ind), 'net', 'outputs', 'performance', 'percentCorrect', 'trainFcn2', 'hiddenLayerSize2');
+        [net,outputs, performance, percentCorrect, trainFcn2, hiddenLayerSize2] ...
+            = pattern_network(in, out, t, h); %#ok<*ASGLU>
+        save(sprintf('%s%d', '2network', ind), 'net', 'outputs', 'performance', ...
+            'percentCorrect', 'trainFcn2', 'hiddenLayerSize2');
         ind = ind + 1;
         close all;
     end
 end
-
 end
